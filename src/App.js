ad2c8dd456447694c9ec9b061fcd7d3e4662b636
Mohan Kumar <mohankr2006@gmail.com>
2024-02-26 11:30:07 +0530

    usequery implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-
-function App() {
+import { useQuery } from "react-query";
+const fetchUsers = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  return res.json();
+};
+const App = () => {
+  const { data, status } = useQuery("users", fetchUsers);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {status === "error" && <p>Error fetching data</p>}
+      {status === "loading" && <p>Fetching data...</p>}
+      {status === "success" && (
+        <div>
+          {data.map((user) => (
+            <p key={user.id}>{user.name}</p>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
-
+};
 export default App;
